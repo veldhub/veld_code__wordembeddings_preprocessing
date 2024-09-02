7e25544ea2dd5e2666ff5ba6891f1136214e4647
cuda@jupyter <cuda@jupyter.arz.oeaw.ac.at>
2024-09-02 10:27:24 -0400

    refactor naming and mounting scheme

--- src/preprocess_strip.sh
+++ src/preprocess_strip.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+in_file_path="/veld/input/${in_file}"
+out_file_path="/veld/output/${out_file}"
+
 if [[ $line_start != "" && $line_end != "" ]] ; then
-  sed -n "${line_start},${line_end}p" $input_file_path > $output_file_path
+  sed -n "${line_start},${line_end}p" $in_file_path > $out_file_path
 elif [[ $line_start != "" && $line_end == "" ]] ; then
-  sed -n "${line_start},\$p" $input_file_path > $output_file_path
+  sed -n "${line_start},\$p" $in_file_path > $out_file_path
 elif [[ $line_start == "" && $line_end != "" ]] ; then
-  sed -n "1,${line_end}p" $input_file_path > $output_file_path
+  sed -n "1,${line_end}p" $in_file_path > $out_file_path
 elif [[ $line_start == "" && $line_end == "" ]] ; then
   echo "error: no line_start and line_end variables giving. Aborting."
 fi
