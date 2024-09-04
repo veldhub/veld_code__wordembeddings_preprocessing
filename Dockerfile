2cccae25c84df24f8dd3368c4e0775e5dbdd6e2a
cuda@jupyter <cuda@jupyter.arz.oeaw.ac.at>
2024-09-04 18:03:37 -0400

    rework veld_preprocess_lowercase

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM debian:bookworm-20240812
 RUN apt update
 RUN apt install -y python3
+RUN apt install -y python3-pip
+RUN pip3 install --break-system-packages ipdb
 
--- src/preprocess_lowercase.py
+++ src/preprocess_lowercase.py
@@ -1,8 +1,8 @@
 import os
 
 
-INPUT_FILE_PATH = os.getenv("input_file_path")
-OUTPUT_FILE_PATH = os.getenv("output_file_path")
+INPUT_FILE_PATH = "/veld/input/" + os.getenv("in_file")
+OUTPUT_FILE_PATH = "/veld/output/" + os.getenv("out_file")
 
 
 with open(INPUT_FILE_PATH, "r") as f_in:
