c665c7e68ed96e07ac95a0bae87137731cdaf527
SteffRhes <steffrhes@pm.me>
2024-09-09 21:38:08 +0200

    veld_preprocess_lowercase.yaml: improve with more metadata handling

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM debian:bookworm-20240812
 RUN apt update
 RUN apt install -y python3=3.11.2-1+b1
 RUN apt install -y python3-pip=23.0.1+dfsg-1
-RUN pip3 install --break-system-packages ipdb==0.13.13
+RUN pip install --break-system-packages ipdb==0.13.13
+RUN pip install --break-system-packages PyYAML==6.0.2
 
--- src/preprocess_lowercase.py
+++ src/preprocess_lowercase.py
@@ -1,12 +1,41 @@
 import os
+import subprocess
+
+import yaml
 
 
-INPUT_FILE_PATH = "/veld/input/" + os.getenv("in_file")
-OUTPUT_FILE_PATH = "/veld/output/" + os.getenv("out_file")
+IN_TXT_PATH = "/veld/input/" + os.getenv("in_txt_file")
+OUT_TXT_PATH = "/veld/output/" + os.getenv("out_txt_file")
+OUT_DATA_DESCRIPTION = os.getenv("out_data_description")
+OUT_VELD_DATA_YAML_PATH = "/veld/output/veld_data_lowercased.yaml"
 
 
-with open(INPUT_FILE_PATH, "r") as f_in:
-    with open(OUTPUT_FILE_PATH, "w") as f_out:
+veld_data_yaml = {
+    "x-veld": {
+        "data": {
+            "description": OUT_DATA_DESCRIPTION,
+            "topics": "NLP",
+            "contents": [
+                "training data",
+                "raw text",
+            ],
+            "file_type": "json",
+            "additional": {
+                "data size": None,
+            }
+        }
+    }
+}
+
+
+with open(IN_TXT_PATH, "r") as f_in:
+    with open(OUT_TXT_PATH, "w") as f_out:
         for line in f_in:
             f_out.write(line.lower())
+
+result = subprocess.run(["du", "-sh", OUT_TXT_PATH], capture_output=True, text=True)
+data_size = result.stdout.split()[0]
+veld_data_yaml["x-veld"]["data"]["additional"]["data size"] = data_size
+with open(OUT_VELD_DATA_YAML_PATH, "w") as f:
+    yaml.dump(veld_data_yaml, f, sort_keys=False)
 
