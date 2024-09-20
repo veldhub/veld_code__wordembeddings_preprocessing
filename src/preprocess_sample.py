013f533c6180855b77846b2d91961a79b5efcb07
SteffRhes <steffrhes@pm.me>
2024-09-21 00:37:59 +0200

    test

--- src/preprocess_sample.py
+++ src/preprocess_sample.py
@@ -29,19 +29,20 @@ def print_and_log(msg):
 
 
 def get_line_indices():
-    print_and_log("counting lines")
-    line_indices_list = []
-    with open(IN_FILE_PATH, "r") as f_in:
-        for i, line in enumerate(f_in):
-            line_indices_list.append(i)
-        print_and_log(f"total_line_count: {len(line_indices_list)}")
-    print_and_log("creating index list of random sample lines")
-    absolute_sample = int((len(line_indices_list) / 100) * PERCENTAGE_SAMPLE)
-    if SAMPLE_RANDOM_SEED is not None:
-        random.seed(SAMPLE_RANDOM_SEED)
-    rand_indices = sorted(random.sample(line_indices_list, absolute_sample))
-    print_and_log(f"number of randomly sampled lines: {len(rand_indices)}")
-    return rand_indices
+    return list(range(10000))
+    # print_and_log("counting lines")
+    # line_indices_list = []
+    # with open(IN_FILE_PATH, "r") as f_in:
+    #     for i, line in enumerate(f_in):
+    #         line_indices_list.append(i)
+    #     print_and_log(f"total_line_count: {len(line_indices_list)}")
+    # print_and_log("creating index list of random sample lines")
+    # absolute_sample = int((len(line_indices_list) / 100) * PERCENTAGE_SAMPLE)
+    # if SAMPLE_RANDOM_SEED is not None:
+    #     random.seed(SAMPLE_RANDOM_SEED)
+    # rand_indices = sorted(random.sample(line_indices_list, absolute_sample))
+    # print_and_log(f"number of randomly sampled lines: {len(rand_indices)}")
+    # return rand_indices
 
 
 def single_process(p_id, individual_list):
